9d270473136ddbfd0c49760b4e47d5c211b390db
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-06-13 10:56:13 -0700

    burn it all

--- showup/src/theme/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  temporaryThing: {
-    foo: "blue",
-  },
-};
--- showup/src/views/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Contributor } from "./contributor";
-export { default as Landing } from "./landing";
-export { default as Login } from "./login";
-export { default as Navigation } from "./navigation";
-export { default as Signup } from "./signup";
-export { default as Router } from "./router";
