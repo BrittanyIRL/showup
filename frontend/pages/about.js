4516fd66b0a3ee431e8c30b31f5ee17c6dced93d
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-10-15 12:17:56 -0700

    header styles i dont like

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -4,25 +4,26 @@ export default function About() {
       <div>
         <h2 className="about-header">About This Project</h2>
         <p>
-          Over the last 2 decades I've lived in 3 major cities, most of which
-          has revolved around going to live events, namely concerts. A major way
-          that I found out about events for smaller shows was by just walking
-          around and seeing posters bands and street teams would put up for
-          events. When I started learning to make apps I thought it would be
-          cool to archive this art instead of being destroyed.
+          Over the last 15 I've lived in 3 major cities, throughout that time a
+          key way for me to find out what shows were coming through was by just
+          walking around and seeing posters bands and street teams would put up
+          for events. When I started learning to make apps I thought it would be
+          cool to archive this art instead of being destroyed. Like everyone, I
+          got busy with my actual career and this project got sidelined for long
+          enough.
         </p>
         <p>
           So now we're here. This is Show Up! I invite you to add in your own
-          posters for your band, venue, or were commissioned to do by creating
-          an account and filling in forms. Otherwise, treat this site as you
-          would a street pole. Acknowledge that there is no rhyme or reason to
-          the view and that you can't click or share anything - just a place to
-          recognize the beauty and effort all these talented humans put into
-          making art that the rain strips away.
+          posters for your band, venue, or that you were commissioned to do by
+          creating an account and filling in forms. Otherwise, treat this site
+          as you would a street pole. Acknowledge that there is no rhyme or
+          reason to the view and that you can't click or share anything - just a
+          place to recognize the beauty and effort all these talented humans put
+          into making art that the rain strips away.
         </p>
         <p>
           Eventually, with time and contributions, this platform will become
-          more robost. So we can have things like filtering by year or city or
+          more robust. So we can have things like filtering by year or city or
           decade and see how art for marketing shows has changed over time or
           location.
         </p>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,10 @@
-import { useRouter } from "next/dist/client/router";
 import { Posters } from "../components";
+import SvgHeader from "../components/shared/svgHeader";
 
 export default function Index() {
-  return <Posters />;
+  return (
+    <div className="main-page">
+      <Posters /> <SvgHeader />
+    </div>
+  );
 }
